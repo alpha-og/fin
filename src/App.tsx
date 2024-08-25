420f7735b9801ea619641ed4a1fd370d8d540b2e
Athul Anoop <athulanoop2020@gmail.com>
2024-08-25 22:38:53 +0530

    added react-hotkeys-hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,74 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { SearchIcon } from "lucide-react";
 import { getCurrentWindow, LogicalSize } from "@tauri-apps/api/window";
+import { useHotkeys } from "react-hotkeys-hook";
 
 function App() {
   const [result, setResult] = useState<[{ name: string; path: string }] | null>(
     null,
   );
+  const currentWindow = getCurrentWindow();
+  useHotkeys(
+    "escape, ctrl+[",
+    async () => {
+      currentWindow.hide();
+    },
+    {
+      preventDefault: true,
+    },
+  );
+  const clearSearchRef = useHotkeys<HTMLInputElement>(
+    "escape, ctrl+[",
+    (e) => {
+      const target = e.target as HTMLInputElement;
+      if (target.value === "") {
+        currentWindow.hide();
+      }
+      target.value = "";
+      target.blur();
+    },
+    {
+      preventDefault: true,
+      enableOnFormTags: ["INPUT"],
+    },
+  );
+  const inputRef = useRef<HTMLInputElement>(null);
 
   async function updateQuery(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.value.length > 0) {
       setResult(await invoke("get_files", { filter: e.target.value }));
       if (result !== null && result.length > 0) {
         getCurrentWindow().setSize(new LogicalSize(600, 400));
+      } else {
+        getCurrentWindow().setSize(new LogicalSize(600, 50));
       }
     } else {
       setResult(null);
       getCurrentWindow().setSize(new LogicalSize(600, 50));
     }
   }
+
   useEffect(() => {
     getCurrentWindow().setSize(new LogicalSize(600, 50));
     getCurrentWindow().setEffects({ radius: 25, effects: [] });
-    window.addEventListener("keydown", async (e) => {
-      if (e.key === "Escape") {
-        await invoke("hide_app");
-      }
-    });
   }, []);
+  useEffect(() => {
+    clearSearchRef.current = inputRef.current;
+  }, [inputRef]);
   return (
     <div
       data-tauri-drag-region
       className="h-screen w-screen flex flex-col items-center justify-start rounded-2xl bg-zinc-800 overflow-hidden"
     >
-      <div className="w-full p-3 flex flex-row items-center gap-1 rounded-md bg-zinc-800 text-white">
+      <div
+        data-tauri-drag-region
+        className="w-full p-3 flex flex-row items-center gap-1 rounded-md bg-zinc-800 text-white"
+      >
         <SearchIcon />
         <input
-          data-tauri-drag-region
+          ref={inputRef}
           onChange={updateQuery}
           autoCapitalize="off"
           autoComplete="off"
