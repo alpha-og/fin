0ed7a6c12b1d5bbe247361541c778459c52dad78
Athul Anoop <athulanoop2020@gmail.com>
2024-08-30 21:09:33 +0530

    cleaned-up and organised code into associated functions on struct Db

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -22,73 +22,97 @@ impl Default for Db {
 }
 impl Db {
     pub fn init(app: &mut tauri::App, path: &str) {
-        // let connection = Connection::open(format!("{path}/cache.db")).unwrap();
-        let connection = Connection::open(format!("{path}/cache.db"))
+        let connection = Connection::open(format!("{path}/file_system_cache.sqlite"))
             .map_or(None, |connection| Some(connection));
         app.manage(Db {
             connection: Arc::new(Mutex::new(connection)),
         });
         let db_state = app.state::<Db>();
-        let arced_connection = Arc::clone(&db_state.connection);
+        Db::create_tables(&db_state);
+        Db::cache_file_system(&db_state);
+    }
+    fn create_tables(db_state: &State<Db>) {
+        let connection_arc = Arc::clone(&db_state.connection);
         std::thread::spawn(move || {
-            let state_connection = arced_connection.lock().unwrap();
-            if let Some(connection) = state_connection.as_ref() {
-                connection
+            let mut state_connection = connection_arc.lock().unwrap();
+            if let Some(connection) = state_connection.as_mut() {
+                let transaction = connection.transaction().unwrap();
+                transaction
                     .execute(
-                        "create table if not exists file_system(
-        name text not null,
-        path text not null)",
+                        "CREATE TABLE IF NOT EXISTS file_system(
+        name TEXT NOT NULL,
+        path TEXT NOT NULL)",
                         (),
                     )
                     .unwrap();
-                connection
+                transaction
                     .execute(
-                        "create unique index if not exists idx_name on file_system(name);",
+                        "CREATE UNIQUE INDEX IF NOT EXISTS idx_name ON file_system(name);",
                         (),
                     )
                     .unwrap();
-                Self::index_files(connection);
+                transaction.execute("CREATE TABLE IF NOT EXISTS query_history(query TEXT NOT NULL, created_at DEFAULT CURRENT_TIMESTAMP, modified_at DEFAULT CURRENT_TIMESTAMP)", ()).unwrap();
+                transaction
+                    .execute(
+                        "CREATE UNIQUE INDEX IF NOT EXISTS idx_query ON query_history(query);",
+                        (),
+                    )
+                    .unwrap();
+
+                let _ = transaction.commit();
             } else {
                 println!("Database connection could not be acquired");
             }
         });
     }
-    fn index_files(connection: &Connection) {
+    fn index_file_system() -> HashMap<String, String> {
         let mut files = HashMap::new();
         for entry in WalkDir::new("/Users/athulanoop/")
             .min_depth(1)
             .max_depth(5)
             .follow_links(true)
             .into_iter()
+            .filter_entry(|entry| {
+                entry
+                    .file_name()
+                    .to_str()
+                    .map(|file_name| !file_name.starts_with("."))
+                    .unwrap_or(false)
+            })
             .filter_map(Result::ok)
         {
-            if entry
-                .file_name()
-                .to_string_lossy()
-                .to_string()
-                .chars()
-                .next()
-                == Some('.')
-            {
-                continue;
-            }
             files.insert(
                 String::from(entry.file_name().to_string_lossy()),
                 String::from(entry.path().to_string_lossy()),
             );
         }
-        for (file_name, file_path) in files.iter() {
-            if let Err(error) = connection.execute(
-                &format!(
+        files
+    }
+    fn cache_file_system(db_state: &State<Db>) {
+        let connection_arc = Arc::clone(&db_state.connection);
+        std::thread::spawn(move || {
+            let files = Self::index_file_system();
+            let mut state_connection = connection_arc.lock().unwrap();
+            if let Some(connection) = state_connection.as_mut() {
+                let transaction = connection.transaction().unwrap();
+                for (file_name, file_path) in files.iter() {
+                    if let Err(error) = transaction.execute(
+                        &format!(
                     "insert or replace into file_system values ('{file_name}', '{file_path}');"
                 ),
-                (),
-            ) {
-                println!("{error}");
-            } else {
-            };
-        }
+                        (),
+                    ) {
+                        println!("{error} | {file_name} | {file_path}");
+                    } else {
+                    };
+                }
+                let _ = transaction.commit();
+            }
+        });
     }
+    // fn cache_query_history(connection: &mut Connection, queries: Vec<String>) {
+    //     std::thread::spawn(move || {});
+    // }
 }
 
 #[tauri::command]
