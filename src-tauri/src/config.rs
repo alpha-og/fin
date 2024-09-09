7a9a184d0c441a5597cee2cd890ce253f5a9d339
Athul Anoop <athulanoop2020@gmail.com>
2024-09-09 10:14:49 +0530

    improved config loading mechanism to use default config if no config file is found

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 
 use directories::BaseDirs;
 use toml;
@@ -6,7 +6,7 @@ use toml;
 #[derive(serde::Serialize, Debug)]
 pub struct Config {
     configs: HashMap<String, String>,
-    source: Option<PathBuf>,
+    source: PathBuf,
 }
 
 impl Default for Config {
@@ -14,7 +14,8 @@ impl Default for Config {
         let configs = HashMap::new();
         Self {
             configs,
-            source: None,
+            source: PathBuf::from_str("./assets/default-config.toml")
+                .expect("Should be valid path string"),
         }
     }
 }
@@ -24,16 +25,21 @@ impl Config {
         self.load_config();
         dbg!(self);
     }
-    fn load_config(&mut self) {
+    fn set_source(&mut self) {
         if let Some(base_dirs) = BaseDirs::new() {
             let home_dir = base_dirs.home_dir();
-            let config_file_path = home_dir.join(".config/fin/fin.toml");
-            let config_file_contents = std::fs::read_to_string(&config_file_path).unwrap();
-            let parsed_config: std::collections::HashMap<String, String> =
-                toml::from_str(&config_file_contents).expect("Should be valid toml");
-            self.configs = parsed_config;
-            self.source = Some(config_file_path);
-        } else {
+            let source = home_dir.join(".config/fin/fin.toml");
+            if source.exists() {
+                self.source = source;
+            }
         }
+    }
+    fn load_config(&mut self) {
+        self.set_source();
+        let config_file_contents = std::fs::read_to_string(&self.source).unwrap();
+        let parsed_config: std::collections::HashMap<String, String> =
+            toml::from_str(&config_file_contents).expect("Should be valid toml");
+        self.configs = parsed_config;
+        return;
     }
 }
