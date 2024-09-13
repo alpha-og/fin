b511a3138bd3c62f7eaaf51824a1d2544096ecf2
Athul Anoop <athulanoop2020@gmail.com>
2024-09-13 17:43:29 +0530

    fixed bug that caused app to crash on launch due to directory not found error

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -2,6 +2,7 @@ mod keymaps;
 
 use directories::BaseDirs;
 use std::{collections::HashMap, path::PathBuf, str::FromStr};
+use tauri::Manager;
 use toml;
 
 #[derive(serde::Serialize, Debug)]
@@ -23,11 +24,15 @@ impl Default for Config {
 
 impl Config {
     pub fn init(&mut self, app: &tauri::App) {
-        self.load_config();
+        self.load_config(app);
         keymaps::init(app);
         dbg!(self);
     }
-    fn set_source(&mut self) {
+    fn set_source(&mut self, app: &tauri::App) {
+        self.source = app
+            .path()
+            .resolve(&self.source, tauri::path::BaseDirectory::Resource)
+            .expect("Should be valid path");
         if let Some(base_dirs) = BaseDirs::new() {
             let home_dir = base_dirs.home_dir();
             let source = home_dir.join(".config/fin/fin.toml");
@@ -36,9 +41,10 @@ impl Config {
             }
         }
     }
-    fn load_config(&mut self) {
-        self.set_source();
-        let config_file_contents = std::fs::read_to_string(&self.source).unwrap();
+    fn load_config(&mut self, app: &tauri::App) {
+        self.set_source(app);
+        let config_file_contents = std::fs::read_to_string(&self.source)
+            .expect(&format!("{} does not exist", self.source.to_str().unwrap()));
         let parsed_config: std::collections::HashMap<String, String> =
             toml::from_str(&config_file_contents).expect("Should be valid toml");
         self.configs = parsed_config;
