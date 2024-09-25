381fb4a5e520f27f6b502cfbf18ad76b472574dc
Athul Anoop <athulanoop2020@gmail.com>
2024-09-25 16:43:05 +0530

    updated plugin api

--- plugin-api/src/lib.rs
+++ plugin-api/src/lib.rs
@@ -1,7 +1,55 @@
-pub trait Plugin {
+use std::collections::HashMap;
+
+pub trait Plugin: Send {
     fn init(&self);
+    // fn get_plugin_info(&self);
+    fn execute(
+        &self,
+        sender: std::sync::mpsc::Sender<Result<f64, String>>,
+        fn_name: &str,
+        args: Vec<String>,
+    );
+    fn destroy(&self);
 }
 
 pub struct PluginManager {
-    plugins: Vec<Box<dyn Plugin>>,
+    pub plugins: HashMap<String, Box<dyn Plugin>>,
+}
+
+impl Default for PluginManager {
+    fn default() -> Self {
+        Self {
+            plugins: HashMap::new(),
+        }
+    }
+}
+
+impl PluginManager {
+    // pub fn init(&self, plugin_directory: &str) {}
+    pub fn index_third_party_plugins(plugin_directory: &str) {
+        for entry in walkdir::WalkDir::new(plugin_directory)
+            .min_depth(1)
+            .into_iter()
+            .filter_entry(|entry| {
+                let file_name_substrings: Vec<String> = entry
+                    .file_name()
+                    .to_string_lossy()
+                    .split(".")
+                    .into_iter()
+                    .map(|substring| substring.to_string())
+                    .collect();
+                let file_extension = file_name_substrings
+                    .get(file_name_substrings.len() - 1)
+                    .unwrap();
+                file_extension == "so" || file_extension == "lua"
+            })
+            .filter_map(Result::ok)
+        {
+            dbg!(entry.path());
+        }
+    }
+    pub fn register_plugin<T: Plugin + 'static>(&mut self, identifier: &str, plugin: T) {
+        self.plugins
+            .insert(identifier.to_string(), Box::new(plugin));
+    }
 }
