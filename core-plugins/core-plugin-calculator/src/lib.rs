5472296b23dd8d6bc85827a13dcac24540955b2a
Athul Anoop <athulanoop2020@gmail.com>
2024-11-17 00:42:07 +0530

    update: introduced checks to prevent unnecessary updates to results

--- core-plugins/core-plugin-calculator/src/lib.rs
+++ core-plugins/core-plugin-calculator/src/lib.rs
@@ -12,6 +12,17 @@ pub struct CalculatorPlugin {
     client_state: Arc<Mutex<plugin_api::ClientState>>,
 }
 
+#[derive(Clone, Debug, serde::Serialize)]
+struct CalculatorResult {
+    result: f64,
+}
+
+impl plugin_api::SearchResultType for CalculatorResult {
+    fn clone_box(&self) -> Box<dyn plugin_api::SearchResultType> {
+        Box::new(self.clone())
+    }
+}
+
 impl Default for CalculatorPlugin {
     fn default() -> Self {
         Self {
@@ -28,13 +39,34 @@ impl Plugin for CalculatorPlugin {
         println!("Calculator plugin initialized!");
     }
     fn start(&mut self) {
-        let client_state = self
+        let mut client_state = self
             .client_state
             .lock()
             .expect("Failed to lock client state");
 
         let query = client_state.get_search_query();
-        println!("Calculator plugin query: {}", query);
+        let result = CalculatorPlugin::calculate(query);
+        if let Ok(result) = result {
+            if let Some(existing_result) = self.result {
+                if existing_result == result {
+                    return;
+                }
+            }
+            self.result = Some(result);
+        } else {
+            self.result = None;
+        }
+        if let Some(result) = self.result {
+            let existing_results = client_state.get_search_results();
+            let mut new_results = Vec::new();
+            for result in existing_results {
+                new_results.push(result);
+            }
+            new_results.push(plugin_api::SearchResult::Single(Box::new(
+                CalculatorResult { result },
+            )));
+            client_state.update_search_results(new_results);
+        }
     }
     fn get_metadata(&self) -> plugin_api::Metadata {
         plugin_api::Metadata {
