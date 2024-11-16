61369886dc6410d088ff99e91d68b7a4f0b8b5ba
Athul Anoop <athulanoop2020@gmail.com>
2024-11-16 23:46:47 +0530

    update: switched to updated plugin api

--- core-plugins/core-plugin-calculator/src/lib.rs
+++ core-plugins/core-plugin-calculator/src/lib.rs
@@ -1,11 +1,15 @@
 mod token;
 
-use plugin_api::{Event, EventType, Metadata, Plugin, Response};
+use std::sync::{Arc, Mutex};
+
+use plugin_api::Plugin;
 use token::Token;
 
+#[derive(Clone)]
 pub struct CalculatorPlugin {
     result: Option<f64>,
     history: Vec<f64>,
+    client_state: Arc<Mutex<plugin_api::ClientState>>,
 }
 
 impl Default for CalculatorPlugin {
@@ -13,40 +17,40 @@ impl Default for CalculatorPlugin {
         Self {
             result: None,
             history: Vec::new(),
+            client_state: Arc::new(Mutex::new(plugin_api::ClientState::default())),
         }
     }
 }
 
 impl Plugin for CalculatorPlugin {
-    fn get_metadata(&self) -> Metadata {
-        Metadata {
+    fn init(&mut self, client_state_arc: Arc<Mutex<plugin_api::ClientState>>) {
+        self.client_state = client_state_arc;
+        println!("Calculator plugin initialized!");
+    }
+    fn start(&mut self) {
+        let client_state = self
+            .client_state
+            .lock()
+            .expect("Failed to lock client state");
+
+        loop {
+            let query = client_state.get_search_query();
+            println!("Calculator plugin query: {}", query);
+        }
+    }
+    fn get_metadata(&self) -> plugin_api::Metadata {
+        plugin_api::Metadata {
             name: "Calculator".to_string(),
-            description: "A simple calculator".to_string(),
+            description: "A calculator plugin".to_string(),
             icon: None,
             url: None,
         }
     }
-
-    fn get_registered_events(&self) -> Vec<EventType> {
-        let mut events = Vec::new();
-        events.push(EventType::UpdateSearchQuery);
-        events
+    fn destroy(&mut self) {
+        println!("Calculator plugin destroyed!");
     }
-    fn get_response(&self) -> Option<Response> {
-        if let Some(result) = self.result {
-            Some(Response::F64(result))
-        } else {
-            None
-        }
-    }
-
-    fn listen(&mut self, event: &Event) {
-        if let Some(event_data) = &event.data {
-            if let Ok(result) = Self::calculate(&event_data) {
-                self.result = Some(result);
-                self.history.push(result);
-            }
-        }
+    fn clone_box(&self) -> Box<dyn Plugin> {
+        Box::new(self.clone())
     }
 }
 
