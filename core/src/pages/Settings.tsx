7f0da3d0b767bb08950d7945bc81cc679e0d1930
Athul Anoop <athulanoop2020@gmail.com>
2024-11-25 18:42:09 +0530

    update: ensure plugin list is always sorted (asc) and improved styling

--- core/src/pages/Settings.tsx
+++ core/src/pages/Settings.tsx
@@ -6,24 +6,82 @@ import { SearchIcon } from "lucide-react";
 import Icon from "../components/common/Icon";
 import { useSettingsStore } from "../store/settings";
 import { useEffect } from "react";
+import { invoke } from "@tauri-apps/api/core";
 
 function PluginList() {
-  const { plugins, refreshPlugins } = useSettingsStore();
+  const { plugins, refreshPlugins, setSelectedPlugin, selectedPlugin } =
+    useSettingsStore();
   useEffect(() => {
     refreshPlugins();
   }, []);
   return (
     <ul className="w-full h-full flex flex-col justify-start items-center gap-1 overflow-y-scroll overflow-x-hidden no-scrollbar outline-none focus:outline-none">
-      {Object.values(plugins).map((plugin, index) => (
-        <li
-          key={index}
-          className="w-full h-7 flex flex-row justify-start items-center gap-4 text-white rounded-xl outline-none focus:outline-none"
-        >
-          <Icon icon={plugin.metadata.icon} size={20} />
-          <span>{plugin.metadata.name}</span>
-        </li>
-      ))}
+      {Object.values(plugins)
+        .sort((a, b) => a.metadata.name.localeCompare(b.metadata.name))
+        .map((plugin, index) => (
+          <li
+            key={index}
+            className={`w-full h-7 p-2 flex flex-row justify-start items-center gap-4 text-white rounded-lg outline-none focus:outline-none hover:cursor-pointer ${selectedPlugin === plugin.metadata.name && "bg-white/10"}`}
+            onClick={() => setSelectedPlugin(plugin.metadata.name)}
+          >
+            <Icon icon={plugin.metadata.icon} size={20} />
+            <span>{plugin.metadata.name}</span>
+          </li>
+        ))}
     </ul>
+  );
+}
+
+function PluginConfig() {
+  const { updatePlugin, getSelectedPlugin } = useSettingsStore();
+  return (
+    <>
+      {getSelectedPlugin() ? (
+        Object.keys(getSelectedPlugin()!.config).length > 0 ? (
+          <ul className="w-full p-1 flex flex-col justify-start items-center gap-1 overflow-y-scroll overflow-x-hidden no-scrollbar outline-none focus:outline-none border border-white/20 rounded-lg">
+            {Object.entries(getSelectedPlugin()?.config ?? {}).map(
+              (config, index) => (
+                <li
+                  key={index}
+                  className="w-full h-7 px-2 flex flex-row justify-start items-center gap-4 text-white rounded-xl outline-none focus:outline-none"
+                >
+                  <span>{config[0]}</span>
+                  <input
+                    type="text"
+                    placeholder={config[1]}
+                    className="w-full px-2 py-[0.1rem] rounded-md bg-white/5 outline-none focus:outline-none"
+                    onChange={(e) => {
+                      updatePlugin({
+                        ...getSelectedPlugin()!,
+                        config: {
+                          ...getSelectedPlugin()!.config,
+                          [config[0]]: e.target.value,
+                        },
+                      });
+                      console.log(e);
+                      invoke("update_plugin_config", {
+                        pluginName: getSelectedPlugin()!.metadata.name,
+                        key: config[0],
+                        value: e.target.value,
+                      });
+                      console.log(getSelectedPlugin()!.config);
+                    }}
+                  />
+                </li>
+              ),
+            )}
+          </ul>
+        ) : (
+          <div className="w-full h-full flex flex-col justify-center items-center gap-1 text-white">
+            <span>No config available</span>
+          </div>
+        )
+      ) : (
+        <div className="w-full h-full flex flex-col justify-center items-center gap-1 text-white">
+          <span>No plugin selected</span>
+        </div>
+      )}
+    </>
   );
 }
 
@@ -46,9 +104,9 @@ function Settings() {
       <div className="h-3 flex flex-row items-center gap-1 rounded-md bg-zinc-800 text-white">
         <span className="text-white">Settings</span>
       </div>
-      <div className="w-full h-full p-1 flex justify-evenly items-center gap-1 overflow-y-scroll overflow-x-hidden no-scrollbar outline-none focus:outline-none">
+      <div className="w-full h-full px-1 py-2 flex justify-evenly items-center gap-1 overflow-y-scroll overflow-x-hidden no-scrollbar outline-none focus:outline-none">
         {/* plugin list and picker */}
-        <div className="w-48 h-full p-1 pr-3 flex flex-col flex-shrink-0 flex-grow-0 justify-start items-center gap-1 text-sm text-white border-r-[0.07rem] border-white/20">
+        <div className="w-48 h-full pr-3 flex flex-col flex-shrink-0 flex-grow-0 justify-start items-center gap-1 text-sm text-white border-r-[0.07rem] border-white/20">
           {/* search bar */}
           <div className="w-full px-2 py-1 flex flex-row items-center gap-1 rounded-md bg-zinc-800 border border-white/20">
             <SearchIcon size={20} />
@@ -64,11 +122,14 @@ function Settings() {
           </div>
         </div>
         {/* plugin config */}
-        <div className="w-full h-full flex flex-col justify-start items-center gap-1">
-          {/* Top bar */}
+        <div className="w-full h-full pl-2 flex flex-col justify-start items-center gap-1 text-sm text-white">
+          {/* Top bar 
           <div></div>
+          */}
           {/* Settings editor */}
-          <div></div>
+          <div className="w-full h-full flex flex-col justify-start items-center gap-1 overflow-y-scroll overflow-x-hidden no-scrollbar outline-none focus:outline-none">
+            <PluginConfig />
+          </div>
         </div>
       </div>
     </div>
